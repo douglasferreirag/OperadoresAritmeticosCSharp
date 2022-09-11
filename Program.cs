37267149e6e5304e6380f4eb01d3eb019bb28b0f
Douglas <douglas.ferreirag@gmail.com>
2022-09-11 20:47:52 -0300

    Incremento e decremento

--- Program.cs
+++ Program.cs
@@ -8,11 +8,22 @@ Calculadora  calc = new Calculadora();
 // calc.Multiplicar(15,45);
 // calc.Dividir(2,2);
 //calc.Potencia(3,3);
-calc.Seno(30);
-calc.Cosseno(30);
-calc.Tangente(30);
+// calc.Seno(30);
+// calc.Cosseno(30);
+// calc.Tangente(30);
 
+int numeroIncremento = 10;
+Console.WriteLine(numeroIncremento);
+Console.WriteLine("Incrementando o 10");
+// numero = numero + 1;
+numeroIncremento++;
+Console.WriteLine(numeroIncremento);
 
+int numeroDecremento = 20;
+Console.WriteLine("Decrementando o 20");
+// numero = numero + 1;
+numeroDecremento--;
+Console.WriteLine(numeroDecremento);
 
 
 
